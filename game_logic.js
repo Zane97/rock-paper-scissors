2c1f7a6bc17fa416b4dd6b854211a29383639254
Zane97 <zanejvv1997@gmail.com>
2020-11-19 21:07:48 +0200

    Moved js into seprate folder

---
+++ game_logic.js
@@ -0,0 +1,56 @@
+ 
+const RPS = [
+    "rock",
+    "paper",
+    "scissors"
+];
+
+// Plays a random element
+function computerPlay() {
+    let comp_play = Math.floor(Math.random() * RPS.length);
+    return comp_play;
+}
+
+
+// Determins the winner
+function round(plr_slct, cmptr_slct) {
+    let outcome = plr_slct - cmptr_slct;
+    
+    // Tie
+    if (outcome == 0) {
+        return `It's a tie, both selected ${RPS[plr_slct]}`;
+    
+    // Player win
+    } else if (outcome == 1 || outcome == -2){
+        return `You win, ${RPS[plr_slct]} beats ${RPS[cmptr_slct]}`;
+    
+    // Player loses
+    } else if (outcome == -1 || outcome == 2) {
+        return `You lost, ${RPS[plr_slct]} looses against ${RPS[cmptr_slct]}`;
+    }
+}
+
+// Get player input
+function playerPlay() {
+    let usr_slct = prompt("Rock Paper or Scissors");
+    usr_slct = usr_slct.toLocaleLowerCase();
+    
+    // TODO: Error handeling
+    return RPS.indexOf(usr_slct);
+}
+
+function game() {
+    let plr_slct = playerPlay();
+    console.log(plr_slct);
+    let cmptr_slct = computerPlay();
+    console.log(cmptr_slct);
+    let result = round(plr_slct, cmptr_slct);
+    console.log(result);
+}
+
+game();
+game();
+game();
+game();
+game();
+game();
